5bd6b8c30c31c1876fe41409cc81597ca8948e48
Archana Krishna <150210401+syrnxalno@users.noreply.github.com>
2025-06-17 21:12:14 +0530

    minimal lomutoPartition.java

--- sorting_techniques/Lomuto Partitioning/lomutoPartition.java
+++ sorting_techniques/Lomuto Partitioning/lomutoPartition.java
@@ -1,1 +1,29 @@
+public class lomutoPartition {
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+    }
+
+    static int partition(int[] arr, int start, int end) {
+        int pivot = arr[end], i = start - 1;
+        for (int j = start; j < end; j++)
+            if (arr[j] < pivot) swap(arr, ++i, j);
+        swap(arr, i + 1, end);
+        return i + 1;
+    }
+
+    static void quickSort(int[] arr, int start, int end) {
+        if (start < end) {
+            int p = partition(arr, start, end);
+            quickSort(arr, start, p - 1);
+            quickSort(arr, p + 1, end);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 56, 8, 75, 25, 22};
+        quickSort(arr, 0, arr.length - 1);
+        for (int num : arr) System.out.print(num + " ");
+        System.out.println();
+    }
+}
 
