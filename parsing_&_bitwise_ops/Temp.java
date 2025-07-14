b95a9f7f83302681e5876fc683e0983a19328681
syrnxalno <archanakrishna0023@gmail.com>
2025-07-14 13:16:35 +0530

    feat(techniques): parsing & bitwise ops

---
+++ parsing_&_bitwise_ops/Temp.java
@@ -0,0 +1,24 @@
+public class Temp {
+    public static void main(String[] args) {
+        String input = "(()(()))((())())(()(()(())))()()((()))";
+        removeOuterParentheses(input); 
+    }
+
+    public static void removeOuterParentheses(String input) {
+        // Track depth to skip outermost parentheses of each primitive group
+        int depth = 0;
+        StringBuilder sol = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            if (c == '(') {
+                if (depth > 0)
+                    sol.append(c);
+                depth++;
+            } else if (c == ')') {
+                depth--;
+                if (depth > 0)
+                    sol.append(c);
+            }
+        }
+        System.out.println(sol);
+    }
+}
