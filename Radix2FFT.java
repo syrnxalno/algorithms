9eac97624d5999fe9cd1a743e2812a3a47a39310
syrnxalno <archanakrishna0023@gmail.com>
2025-08-17 20:34:14 +0530

    radix-2 fast fourier transform

---
+++ Radix2FFT.java
@@ -0,0 +1,18 @@
+/*
+ * The Real and Complex form of DFT (Discrete Fourier Transforms) can be used to perform frequency analysis or synthesis for any discrete and periodic signals.
+ * The FFT (Fast Fourier Transform) is an implementation of the DFT which may be performed quickly on modern CPUs.
+ * The simplest and perhaps best-known method for computing the FFT is the Radix-2 Decimation in Time algorithm, attempted below
+ */
+
+/*
+ * Some Standard constants
+ * PI = 3.1415926535897932384626433832795 // PI for sine/cos calculations
+ * TWOPI = 6.283185307179586476925286766559 // 2*PI for sine/cos calculations
+ * Deg2Rad = 0.017453292519943295769236907684886 // Degrees to Radians factor
+ * Rad2Deg = 57.295779513082320876798154814105 // Radians to Degrees factor
+ * log10_2 = 0.30102999566398119521373889472449 // Log10 of 2
+ * log10_2_INV = 3.3219280948873623478703194294948 // 1/Log10(2)
+ */
+public class Radix2FFT {
+    
+}
