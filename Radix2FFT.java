89dc1ab04728fabc96c3a93862caec08cb7fd8dd
syrnxalno <archanakrishna0023@gmail.com>
2025-08-17 20:42:03 +0530

    isPowerOfTwo()

--- Radix2FFT.java
+++ Radix2FFT.java
@@ -14,5 +14,17 @@
  * log10_2_INV = 3.3219280948873623478703194294948 // 1/Log10(2)
  */
 public class Radix2FFT {
-    
+    public static void main(String args[]) {
+        double log10_2_INV = 3.3219280948873623478703194294948;
+        isPowerOfTwo(0, 0, log10_2_INV);
+    }
+
+    public static boolean isPowerOfTwo(int N, int M, double val) {
+        M = (int) Math.ceil(Math.log10((double) N) * val);
+        int NN = (int) Math.pow(2.0, M);
+
+        if ((NN != N) || (NN == 0)) // Check N is a power of 2.
+            return false;
+        return true;
+    }
 }
